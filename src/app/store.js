f96416b73b6f7f2386d9429099b43ae1855a6f31
parmar-prachi <parmarprachi.1812@gmail.com>
2026-03-22 20:28:29 +0530

    commit

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import apiReducer from "../features/api/apiSlice";
-
-export const store = configureStore({
-    reducer:{
-        api:apiReducer,
-    }
-});
